d9d01c481872352a61eb8914a61c0084818bc6a9
GuiSNC <guilhermepereira.gui11@gmail.com>
2025-01-17 22:47:53 +0000

    criei porto alegre

--- cartasSuperTrunfo.c
+++ cartasSuperTrunfo.c
@@ -8,11 +8,22 @@ int main(){
     float pib;
     int pontosturisticos;
 
-    printf("A\n");
-    printf("- Cidade: São Paulo\n- Codigo Postal: 01153-000\n- População: 11.895.578\n- Área:1.523 km² \n- PIB: R$ 828.980.607,731  \n- Pontos Turísticos: 36  \n");
+    printf("A1\n");
+    printf("- Cidade: São Paulo\n- Codigo Postal: 01153-000\n- População: 11.895.578\n- Área:1.523 km² \n- PIB: R$ 828,98 bilhões \n- Pontos Turísticos: 36  \n");
 
-    printf("B\n");
-    printf("\n- Cidade: Goiânia \n- Codigo Postal: 72800-000 \n- População: 2.654.860 \n- Área: 728,8 km² \n- PIB: R$ 59.865.989,619  \n- Pontos Turísticos: 42 \n");
+    printf("\nA2\n");
+    printf("\n- Cidade: Goiânia \n- Codigo Postal: 72800-000 \n- População: 2.654.860 \n- Área: 728,8 km² \n- PIB: R$ 8,149 bilhões \n- Pontos Turísticos: 42 \n");
+
+    printf("\nA3\n");
+    printf("\n- Cidade: Rio Janeiro \n- Codigo Postal: 20230-010 \n- População: 6.211.000 \n- Área: 1.200 km² \n- PIB: R$ 37,784 bilhões  \n- Pontos Turísticos: 58 \n");
+    
+    printf("\nA4\n");
+    printf("\n- Cidade: Porto Alegre \n- Codigo Postal: 90010-420 \n- População: 1,333 milhões \n- Área: 496,8 km² \n- PIB: R$ 9,787 bilhões  \n- Pontos Turísticos: 40 \n");
+
+
+
+
+    
     scanf("%s", &cidade);
     scanf("%d", &codigopostal);
     scanf("%d", &populacao);
@@ -20,12 +31,6 @@ int main(){
     scanf("%f", &pib);
     scanf("%d", &pontosturisticos);
 
-    scanf("%s", &cidade);
-    scanf("%d", &codigopostal);
-    scanf("%d", &populacao);
-    scanf("%f", &area);
-    scanf("%f", &pib);
-    scanf("%d", &pontosturisticos);
 
     return 0;
 
