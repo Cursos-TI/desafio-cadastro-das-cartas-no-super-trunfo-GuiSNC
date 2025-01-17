d1631e559503810a9500b9f636ef0d7c63fc17eb
GuiSNC <guilhermepereira.gui11@gmail.com>
2025-01-17 19:11:52 +0000

    criei 2 cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@
 
 int main() {
     printf("Desafio do jogo trunfo!");
-
+    
 
 
 
---
+++ cartasSuperTrunfo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    char cidade[20];
+    float codigopostal;
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+
+    printf("A\n");
+    printf("- Cidade: São Paulo\n- Codigo Postal: 01153-000\n- População: 11.895.578\n- Área:1.523 km² \n- PIB: R$ 828.980.607,731  \n- Pontos Turísticos: 36  \n");
+
+    printf("B\n");
+    printf("\n- Cidade: Goiânia \n- Codigo Postal: 72800-000 \n- População: 2.654.860 \n- Área: 728,8 km² \n- PIB: R$ 59.865.989,619  \n- Pontos Turísticos: 42 \n");
+    scanf("%s", &cidade);
+    scanf("%d", &codigopostal);
+    scanf("%d", &populacao);
+    scanf("%f", &area);
+    scanf("%f", &pib);
+    scanf("%d", &pontosturisticos);
+
+    scanf("%s", &cidade);
+    scanf("%d", &codigopostal);
+    scanf("%d", &populacao);
+    scanf("%f", &area);
+    scanf("%f", &pib);
+    scanf("%d", &pontosturisticos);
+
+    return 0;
+
+
+    
+}
